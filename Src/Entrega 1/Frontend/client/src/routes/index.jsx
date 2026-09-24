edf879cca74355afab91f188f957be6c7d184557
GabrielOliveiraFecap <gabriel.oliveira4@edu.fecap.br>
2026-09-24 20:01:13 -0300

    consertando conflito

--- Src/Entrega 1/Frontend/client/src/routes/index.jsx
+++ Src/Entrega 1/Frontend/client/src/routes/index.jsx
@@ -5,12 +5,11 @@ import LandingPage from '../pages/LandingPage'
 import ValidacaoEmail from '../pages/ValidacaoEmail'
 import NotFound from '../pages/NotFound'
 import Cadastro from '../pages/Cadastro'
-import TelaDeEscolha from '../pages/TelaDeEscolha'
 import Login from '../pages/Login'
 import PainelDeEventos from '../pages/PainelDeEventos'
-<<<<<<< HEAD
-import AprovacaoCadastros from '../pages/adminPages/AprovacaoCadastros'
+import AprovarCadastros from '../pages/adminPages/AprovarCadastros'
 import EventRegistration from '../features/eventRegistration/EventRegistration.jsx'
+import AdminDashboard from '../pages/adminPages/AdminDashboard.jsx'
 
 export const router = createBrowserRouter([
   {
@@ -19,10 +18,10 @@ export const router = createBrowserRouter([
       { path: '/', element: <LandingPage /> },
       { path: '/validation', element: <ValidacaoEmail /> },
       { path: '/Cadastro', element: <Cadastro /> },
-      { path: '/TelaDeEscolha', element: <TelaDeEscolha /> },
       { path: '/Login', element: <Login /> },
       { path: '/PainelDeEventos', element: <PainelDeEventos /> },
-      { path: '/AprovacaoCadastros', element: <AprovacaoCadastros /> },
+      { path: '/AprovarCadastros', element: <AprovarCadastros /> },
+      { path: "/AdminDashboard", element: <AdminDashboard/>},
       {
         path: '/criar-evento/:etapa?',
         element: <EventRegistration />,
@@ -30,62 +29,4 @@ export const router = createBrowserRouter([
       { path: '*', element: <NotFound /> },
     ],
   },
-=======
-import AprovarCadastros from '../pages/adminPages/AprovarCadastros'
-import AdminDashboard from '../pages/adminPages/AdminDashboard'
-
-export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <LandingPage/>,
-    },
-    {
-        path: "/validation",
-        element: <ValidacaoEmail/>,
-    },
-    {
-        path: "*",
-        element: <NotFound/>,
-    },
-    {
-        path:"/Cadastro",
-        element:<Cadastro/>,
-    },
-    {
-        path: "/Login",
-        element: <Login/>,
-    },
-    {
-        path: "/PainelDeEventos",
-        element: <PainelDeEventos/>,
-    },
-    {
-        path: "/AprovarCadastros",
-        element: <AprovarCadastros/>,
-    },
-    {
-        path: "/AdminDashboard",
-        element: <AdminDashboard/>,
-    },
-    /*{
-        path: '/evento',
-        element: <EventFlow />,
-      },
-      {
-        path: '/publico-lotes',
-        element: <EventFlow />,
-      },
-      {
-        path: '/custos-independentes',
-        element: <EventFlow />,
-      },
-      {
-        path: '/itens-custos',
-        element: <EventFlow />,
-      },
-      {
-        path: '*',
-        element: <Navigate to="/evento" replace />,
-      },*/
->>>>>>> 29d9cfd7746fb31cecfcde12ad76d9a3c7990606
 ])
